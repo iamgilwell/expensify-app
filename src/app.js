d2000fc34283e1d44943fbaff297dcce07a66df9
Gilwell M <gilwellm@gmail.com>
2020-04-28 14:12:15 +0300

    React Dev Boiler Plate

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 // install -> import -> use;
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>This is My Boilder Plate</p>, document.getElementById("app"));
 
 // media/gilwell / Windows / Tutorials / First Quarter / REACT / The Complete React Developer Course(w Hooks and Redux) / 8. Styling React
 // 10. Mobile Considerations.mp4
--- src/playground/person.js
+++
@@ -1,7 +0,0 @@
-const isAdult = (age) => age >= 18;
-const canDrink = (age) => age >= 21;
-const isSenior = (age) => age >= 65;
-
-
-
-export { isAdult, canDrink, isSenior as default } 
